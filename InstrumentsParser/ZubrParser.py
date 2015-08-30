f55f240240762228dfeb2c0ef942f3e8d09eca1c
Grey <Grey@Grey>
2015-08-30 16:43:58 +0600

    Работает все, кроме парсинга артикулов

--- InstrumentsParser/ZubrParser.py
+++ InstrumentsParser/ZubrParser.py
@@ -9,14 +9,15 @@ import os;
 import re;
 from urllib2 import urlopen;
 import lxml.html as html;
+import lxml.etree as etree;
 
 #-----------------------------------------------------------------
 
-site_url='http://zubr.ru/';
+site_url='http://zubr.ru';
 imagesDir = 'instrument_images';
 CSVFile = 'instrument_parse-results.csv';
 CACHEFile = 'itemlinks.txt';
-
+formatStr = '{0};{1};{2};{3};{4}\n';
 #-------------------------------------------------------------------   
 
 def PrettifyCategoryStr(Str):
@@ -38,21 +39,27 @@ def ParseName(root, tree):
 def ParseDescDiv_spec(root, tree):
     return root.get_element_by_id('specifications');
 
-def ParseDescDiv_features(root, tree):
-    return root.get_element_by_id('features');
+def ParseDescDiv_features(root, tree): 
+    return root.get_element_by_id('features'); 
 
 def ParseDesc(desc_div_spec, desc_div_features, tree):
-    return tree.tostring(desc_div_spec.drop_tag()) + tree.tostring(desc_div_features.drop_tag());
+    res = ''; 
+    for child in desc_div_features.getchildren():
+        res += html.tostring(child).replace(';', ',');
+    for child in desc_div_spec.getchildren():
+        res += html.tostring(child).replace(';', ',');
+    #print res;
+    return res;
 
 def ParseImages(root, tree):
     res = [];
-    images = root.find_class('gallery__image');
-    for image in images:
-        for elem in image.getchildren():
-            if elem.tag == 'a':
-                res.append(elem.get('href'));
-                
-    return res;
+    gallery = root.find_class('item-gallery');
+    for galleryClass in gallery:
+        for imageLink in galleryClass.iterlinks():
+            if 'type=resize&w=800&h=600' in imageLink[2]:
+                res.append(site_url + imageLink[2]);
+    resStr = ';'.join(res); 
+    return resStr;
 
 def ParseCategory(root, tree):
     box = root.get_element_by_id('content-box');
@@ -68,23 +75,25 @@ def ParseCategory(root, tree):
                 way += '|';
     return way;
 
-def savepic(img, folderPath):
-    PathParts = folderPath.split('/');
-    Path = imagesDir;
-    for pathPart in PathParts:
-        Path += pathPart; 
-        if not os.path.exists(Path):
-            os.makedirs(Path);
-    resource = urlopen(site_url + img);
-    urlparts = img.split('/');
-    imagename = "{0}\{1}".format(folderPath, urlparts[-1]);
-    if not os.path.exists(imagename):
-        out = open(imagename, 'wb');
-        out.write(resource.read());
-        out.close(); 
-        print imagename + ' saved';
-    else:
-        print imagename + ' passed';
+def savepics(imgs, itemLink):
+    itemLink = itemLink.strip().replace('/', '\\');
+    fullPath = r'images\zubr' + itemLink;
+    if not os.path.exists(fullPath):
+        os.makedirs(fullPath);
+    for img in imgs.split(';'):
+        resource = urlopen(img);
+        #urlparts = img.split('/');
+        
+        imagename = "{0}\\{1}".format(fullPath, itemLink.split('\\')[-1] + '.png');
+        if not os.path.exists(imagename):
+            out = open(imagename, 'wb');
+            try:
+                out.write(resource.read());
+            finally:
+                out.close(); 
+            print imagename + ' saved';
+        else:
+            print imagename + ' passed';
 
 def IsItemsCached():
     return os.path.exists(CACHEFile);
@@ -128,7 +137,7 @@ def CacheItems():
 
 f = open(CSVFile, 'w');
 try:
-    f.write('{0};{1};{2};{3};{4}\n'.format('sku', 'name', 'desc', 'group', 'img', 'adImg1', 'adImg2', 'adImg3'));
+    f.write('{0};{1};{2};{3};{4};{5};{6};{7};{8};\n'.format('sku', 'name', 'desc', 'group', 'img', 'adImg1', 'adImg2', 'adImg3', 'adImg4'));
     if not IsItemsCached():
         CacheItems();                  
     items_cache = open(CACHEFile, 'r');
@@ -144,8 +153,8 @@ try:
             print 'Category:' + group_str;
             img_str = ParseImages(root, tree);
             print 'Images:' + img_str;
-            if not (img_str != ''):
-                savepic(img_str, group_str);
+            if img_str != '':
+                savepics(img_str, itemLink);
                 
             desc_div_features = ParseDescDiv_features(root, tree);    
             desc_div_spec = ParseDescDiv_spec(root, tree);
@@ -153,15 +162,15 @@ try:
             # основная операция
             SKUs_NameDesc_dict = ParseSKU_DESC(desc_div_spec, tree);
             
-            desc_str = ParseDesc(desc_div_features, desc_div_spec, tree);
-            
-            for sku_str, name_additional  in SKUs_NameDesc_dict:
-                f.write('{0};{1};{2};{3};{4}\n'.format(sku_str, 
-                                                       '{0}({1})'.format(name_str, name_additional), 
-                                                       desc_str,
-                                                       group_str, 
-                                                       img_str));
-            print itemLink.strip();
+            desc_str = ParseDesc(desc_div_spec, desc_div_features, tree);
+            #print desc_str;
+            '''for sku_str, name_additional  in SKUs_NameDesc_dict:
+                f.write(formatStr.format(sku_str, 
+                                        '{0}({1})'.format(name_str, name_additional), 
+                                        desc_str,
+                                        group_str, 
+                                        img_str));'''
+            #print itemLink.strip();
         items_cache.close();  
     except:
         items_cache.close();   
